46f344e83cc69dafd62326b0a83478e3c0349fff
g <g@g.(none)>
2013-10-02 13:49:38 -0400

    fast commit

--- vmxapp-postmark.js
+++ vmxapp-postmark.js
@@ -18,8 +18,8 @@ VMX.callback = function(detections){
   var now = new Date().getTime();
   if(detections[0].score > 1){
     if(!last || now > last + 5000){
-      last = new Date().getTime();
       console.log("only every five seconds!",last,now);
+      console.log($http);
     }
   }
 }
