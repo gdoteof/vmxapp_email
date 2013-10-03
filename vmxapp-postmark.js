9bb5ee75a770a7a8f6e99d0de9aab925d33d2886
g <g@g.(none)>
2013-10-03 01:21:31 -0400

    fast commit

--- vmxapp-postmark.js
+++ vmxapp-postmark.js
@@ -2,13 +2,14 @@ var api_key= "5efd4b21-806d-4d05-a034-bb0fed6f9f07"
 
 
 var url =  "http://192.168.0.104/v/email.php";
-var headers = [];
+var headers = {};
 headers["Accept"] = "application/json";
 headers["Content-Type"] = "application/json";
 headers["X-Postmark-Server-Token"] = "ed742D75-5a45-49b6-a0a1-5b9ec3dc9e5d";
 var msg = {
     From: 'geoff@vmx.ai',
-    Headers: headers,
+    Headers: [headers],
+    test: [1,2,3],
     To  : 'geoffgolder@gmail.com',
     Subject: "Hello from VMX APP",
     HtmlBody: "thsse bodyxx",
