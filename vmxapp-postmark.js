4be038c8261bd1cf420bdbd8ebd2f6cf31977255
g <g@g.(none)>
2013-10-03 02:32:24 -0400

    fast commit

--- vmxapp-postmark.js
+++ vmxapp-postmark.js
@@ -1,6 +1,10 @@
-var last = 0;
+var last = 0;  //set init time to 0
 VMX.callback = function(detections){
+  // a local script for sending email
   var url =  "http://192.168.0.104/v/email.php";
+
+
+  //The message, roughly formatted as postmarkapp wants it
   var msg = {
       From: 'geoff@vmx.ai',
       To  : 'geoffgolder@gmail.com',
@@ -12,7 +16,10 @@ VMX.callback = function(detections){
   if(detections[0].score > 1){
     if(!last || now > last + 5000){   //do this a max of once every 5 seconds
       last = new Date().getTime();
-      $.post(url,msg).success(function(response){
+
+      //USE ANGULAR MODULES!  
+      //could also be $.post(...) (jquery works as well)
+      $http.post(url,msg).success(function(response){
         console.log("success!",response);
       }).error(function(response){
         console.log("error", response); 
